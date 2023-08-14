d91942a2a462a65fa3daaf516cdfee518cbf1576
Willy Chen <willy810405@gmail.com>
2023-08-14 13:20:35 +0800

    fix

--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -3,3 +3,17 @@ variable "secret_key" {}
 variable "region" {
    default = "ap-northeast-1"
 }
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type 	      = string
+  default     = "Test_EC2_from_Terraform"
+}
+
+variable "ami_id" {
+  description = "Canonical, Ubuntu, 22.04 LTS, amd64 jammy image build on 2023-05-16"
+  type        = string 
+  default     = "ami-0d52744d6551d851e"
+}
+
+
